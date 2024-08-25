f0235ff625d9d67526b5355c391d8e8e32306426
NANDISHSHAH <nandishshahwork@gmail.com>
2024-08-25 23:15:07 +0530

    Hello world project

---
+++ helloworld/target/debug/deps/helloworld-e8d2b991ebe19b03.d
@@ -0,0 +1,5 @@
+/Users/nandishshah/Desktop/rustlearn-/helloworld/target/debug/deps/helloworld-e8d2b991ebe19b03: src/main.rs
+
+/Users/nandishshah/Desktop/rustlearn-/helloworld/target/debug/deps/helloworld-e8d2b991ebe19b03.d: src/main.rs
+
+src/main.rs:
---
+++ helloworld/target/debug/helloworld.d
@@ -0,0 +1,1 @@
+/Users/nandishshah/Desktop/rustlearn-/helloworld/target/debug/helloworld: /Users/nandishshah/Desktop/rustlearn-/helloworld/src/main.rs
